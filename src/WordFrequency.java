ba2085807669880224a916db2b68696e0b64276c
Katherine Watkins <watkins.katherine@student.greenriver.edu>
2023-02-19 16:57:51 -0800

    word frequency method added

--- src/WordFrequency.java
+++ src/WordFrequency.java
@@ -1,5 +1,44 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class WordFrequency {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void wordFrequency(String fileName) {
+        Map<String, Integer> wordMap = new HashMap<>();
+
+
+        try (Scanner s = new Scanner(new File(fileName))) {
+            while (s.hasNext()) {
+                String current = s.next();
+
+                if(wordMap.containsKey(current)){
+                    int currentCount = wordMap.get(current);
+                    currentCount++;
+                    wordMap.put(current, currentCount);
+                }else{
+                    wordMap.put(current, 1);
+                }
+
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + fileName);
+        }
+
+        for(String key : wordMap.keySet()){
+            System.out.println(key + " appears " + wordMap.get(key) + " times");
+        }
+
     }
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        wordFrequency("tinyTale.txt");
+    }
+
+
+
+
 }
