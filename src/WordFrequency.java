c0b709132de95cbfa160e0a87f02931ad47ab9a0
Katherine Watkins <watkins.katherine@student.greenriver.edu>
2023-02-19 19:12:47 -0800

    added method to find most used word or words

--- src/WordFrequency.java
+++ src/WordFrequency.java
@@ -29,13 +29,50 @@ public class WordFrequency {
         for(String key : wordMap.keySet()){
             System.out.println(key + " appears " + wordMap.get(key) + " times");
         }
+        Map<String, Integer> maxWords = findMax(wordMap);
+        if(maxWords.size() > 1){
+            System.out.print("The words that appear the most times are");
+            int count = maxWords.size();
+            for(String key : maxWords.keySet()){
+                System.out.print(" \"" + key + "\"");
+                if(count > 2){
+                    System.out.print(",");
+                }
+                else if(count > 1){
+                    System.out.print(", and");
+                }
+                count--;
+            }
+            System.out.println(".");
+        }else{
+            System.out.print("The word that appears the most times is ");
+            for(String key : maxWords.keySet()) {
+                System.out.println("\"" + key + "\".");
+            }
+        }
 
+    }
+
+    public static Map<String, Integer> findMax(Map<String, Integer> thisMap){
+        int max = 0;
+        Map<String, Integer> maxMap = new HashMap<>();
+        for(String key : thisMap.keySet()){
+            if(thisMap.get(key) > max){
+                max = thisMap.get(key);
+                maxMap = new HashMap<>();
+                maxMap.put(key, thisMap.get(key));
+
+            }else if(thisMap.get(key) == max){
+                maxMap.put(key, thisMap.get(key));
+            }
+        }
+        return maxMap;
     }
 
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        wordFrequency("tinyTale.txt");
+        wordFrequency("medTale.txt");
     }
 
 
