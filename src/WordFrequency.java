7f1803af9954a3a31506faa59b4052607b65bbaf
Ron Nguyen <ronnguyen1011@gmail.com>
2023-02-21 21:28:29 -0500

    Finish

--- src/WordFrequency.java
+++ src/WordFrequency.java
@@ -1,5 +1,26 @@
+import java.util.*;
+import java.io.*;
+
 public class WordFrequency {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException {
+        Map<String, Integer> wordCounts = new TreeMap<>();
+
+        Scanner infile = new Scanner(new File("tinyTale.txt").getAbsolutePath());
+
+        while(infile.hasNext()){
+            String word = infile.next();
+
+            if (wordCounts.containsKey(word)){
+                int value = wordCounts.get(word);
+                value++;
+                wordCounts.put(word, value);
+            }
+            else {
+                wordCounts.put(word, 1);
+            }
+        }
+
+        System.out.println(wordCounts);
     }
+
 }
