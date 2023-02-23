d7e813984ce34de34d49046e08c4c53a72cbf16f
cloverspark <alexbrenna1999@hotmail.com>
2023-02-22 16:59:27 -0800

    word frequency counter working and implemented like in class.

--- src/WordFrequency.java
+++ src/WordFrequency.java
@@ -1,5 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 public class WordFrequency {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void wordFrequency(String fileName) {
+        Map<String, Integer> wordMap = new TreeMap<>();
+        //hashmap = randomly
+        //treemap = orderly alphabetically
+
+        try (Scanner s = new Scanner(new File(fileName))) {
+            while (s.hasNext()) {
+                String current = s.next();
+
+                if (wordMap.containsKey(current)) {
+                    int currentCount = wordMap.get(current);
+                    currentCount++;
+                    wordMap.put(current, currentCount);
+                } else {
+                    wordMap.put(current, 1);
+                }
+
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + fileName);
+        }
+
+        for (String key : wordMap.keySet()) {
+            System.out.println(key + " appears " + wordMap.get(key) + " times");
+        }
+
     }
+    public static void main(String[] args) {
+        wordFrequency("tinyTale.txt");
+    }
+
 }
