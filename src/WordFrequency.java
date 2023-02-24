1211f914b1c95d4bad20cfa396b0cca9befac09e
Brandon Burgess <burgess.brandon@student.greenriver.edu>
2023-02-24 17:06:25 -0500

    added first loops

--- src/WordFrequency.java
+++ src/WordFrequency.java
@@ -1,5 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class WordFrequency {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Map<String, Integer> wordCounts = new TreeMap<>();
+
+        Scanner inFile = new Scanner(new File("tale.txt"));
+
+        while(inFile.hasNext()) {
+            String word = inFile.next();
+
+            if(wordCounts.containsKey(word)) {
+                //increment the count
+                int value = wordCounts.get(word);
+
+                value++;
+
+                wordCounts.put(word, value);
+            }
+            else {
+                //add word to the map with value of 1
+                wordCounts.put(word,1);
+            }
+        }
     }
 }
